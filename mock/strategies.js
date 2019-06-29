97e044efc4eecd65c13bf03deafd0920ebbfaae7
Li Dong <lidongbest5@gmail.com>
2019-06-29 23:19:52 +0800

    add strategies mock data

--- mock/strategies.js
+++ mock/strategies.js
@@ -1,14 +1,75 @@
 const faker = require("faker");
 const _ = require("lodash");
 
-const generator = ({ count = 100 }) => {
-  return _.range(count).map(i => {
-    return {
-      id: faker.random.uuid(),
-      createdAt: faker.date.past(), // 创建时间
-      updatedAt: faker.date.past(), // 更新时间
-    };
-  });
+const generator = () => {
+  return [
+    {
+      id: 1,
+      name: "1#冷机",
+      data: [
+        {
+          start: "06: 00",
+          end: "10: 00",
+          left: "25%",
+          width: "15%",
+          color: "#fad337",
+        },
+        {
+          start: "14: 00",
+          end: "18: 00",
+          left: "55%",
+          width: "15%",
+          color: "#fcb96e",
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "2#冷机",
+      data: [
+        {
+          start: "12: 00",
+          end: "18: 00",
+          left: "50%",
+          width: "25%",
+          color: "#ff1712",
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "1#冷水泵",
+      data: [
+        {
+          start: "8: 00",
+          end: "16: 00",
+          left: "30%",
+          width: "50%",
+          color: "#fcb96e",
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: "1#冷却塔",
+      data: [
+        {
+          start: "8: 00",
+          end: "16: 00",
+          left: "10%",
+          width: "25%",
+          color: "#fcb96e",
+        },
+        {
+          start: "8: 00",
+          end: "16: 00",
+          left: "60%",
+          width: "20%",
+          color: "#ff1712",
+        },
+      ],
+    },
+  ];
 };
 
 module.exports = generator;
