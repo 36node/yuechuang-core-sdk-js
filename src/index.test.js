fcdb31123fa1e4df371749c0b0c10707b8c032e5
Li Dong <lidongbest5@gmail.com>
2019-06-29 22:56:12 +0800

    update test case

--- src/index.test.js
+++ src/index.test.js
@@ -7,25 +7,25 @@ describe("## SDK vehicle", () => {
   it("should list clusters", async () => {
     const result = await sdk.airConditioner.listClusters();
 
-    expect(result.body.length).toBe(100);
+    expect(result.body.id).toBe(1);
   });
 
   it("should list hosts", async () => {
     const result = await sdk.airConditioner.listHosts();
 
-    expect(result.body.length).toBe(100);
+    expect(result.body.length).toBe(2);
   });
 
   it("should list pumps", async () => {
     const result = await sdk.airConditioner.listPumps();
 
-    expect(result.body.length).toBe(100);
+    expect(result.body.length).toBe(2);
   });
 
   it("should list towers", async () => {
     const result = await sdk.airConditioner.listTowers();
 
-    expect(result.body.length).toBe(100);
+    expect(result.body.length).toBe(2);
   });
 
   it("should list strategies", async () => {
